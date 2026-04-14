bd19ba68011cfd0a2869da361031b5f196ff31dd
rickleandrok-source <rickleandro.k@gmail.com>
2026-04-14 20:13:29 -0300

    Alterei o 'Hello World'

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, VS Code!");
+        System.out.println("Bom dia, o fracasso te espera!");
     }
 }
