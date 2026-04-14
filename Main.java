a154d8d3ed55dbcbe647eed9b78dcbeb3f31690f
leonardo-kefer <leonardo.kefer@escale.com.br>
2026-04-14 20:52:06 -0300

    teste

--- Main.java
+++ Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bom dia, o fracasso te espera!");
         System.out.println("Sei la");
-        System.out.println("Bom dia, o fracasso te espera!");
     }
 }
