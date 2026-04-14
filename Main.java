e9192143b76d99b1da532053612ed51143c4f3a7
leonardo-kefer <leonardo.kefer@escale.com.br>
2026-04-14 20:45:09 -0300

    merge into main

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bom dia, o fracasso te espera!");
         System.out.println("Sei la");
+        System.out.println("Bom dia, o fracasso te espera!");
     }
 }
